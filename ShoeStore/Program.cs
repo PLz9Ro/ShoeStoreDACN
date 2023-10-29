349d1e8ac4e082d8671da73435ad24b14a4efba6
Lam Thanh Phuc <lamthanhphucit@gmail.com>
2023-10-29 18:03:15 +0700

    fix lỗi sql

--- ShoeStore/Program.cs
+++ ShoeStore/Program.cs
@@ -11,20 +11,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 
-// Add services to the container.
-builder.Services.AddDbContext<ShoeStoreContext>(options =>
-{
-    var stringConnectdb = builder.Configuration.GetConnectionString("ShoeStore");
-    options.UseSqlServer(stringConnectdb);
-});
+// Add services to the container.   
+    builder.Services.AddDbContext<ShoeStoreContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ShoeStore"))
+    );
+
+
 builder.Services.AddSingleton<HtmlEncoder>(HtmlEncoder.Create(allowedRanges:new [] {UnicodeRanges.All }));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
